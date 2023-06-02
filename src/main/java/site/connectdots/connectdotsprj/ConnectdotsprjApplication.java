4a12e7739375e645c00ddfed353e2a9a9b897b8c
lkdcode <dlrlejr1@naver.com>
2023-06-02 15:03:49 +0900

    :tada: initial commit

---
+++ src/main/java/site/connectdots/connectdotsprj/ConnectdotsprjApplication.java
@@ -0,0 +1,13 @@
+package site.connectdots.connectdotsprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectdotsprjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectdotsprjApplication.class, args);
+	}
+
+}
