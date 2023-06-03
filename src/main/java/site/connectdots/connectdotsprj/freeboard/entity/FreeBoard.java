980fb0b41cfc5031a61d19780e75ee35742f9cdf
lkdcode <dlrlejr1@naver.com>
2023-06-04 01:59:17 +0900

    :sparkles: [feature] freeBoard
    entity
    1. 조회수, 리플수, 좋아요 수 기본값 설정
    2. 멤버 연관 관계 설정 @ManyToOne
    
    Test
    1. 벌크 데이터 생성
    2. 게시판 전체 조회 테스트 완료
    3. 아이디 조회 테스트 완료
    4. 자유게시판의 작성자를 통해 작성자의 정보를 가져오는 테스트 완료

--- src/main/java/site/connectdots/connectdotsprj/freeboard/entity/FreeBoard.java
+++ src/main/java/site/connectdots/connectdotsprj/freeboard/entity/FreeBoard.java
@@ -4,6 +4,7 @@ import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import site.connectdots.connectdotsprj.member.entity.Member;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -12,7 +13,7 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
+@ToString(exclude = {"member"})
 @Builder
 @Entity
 @EqualsAndHashCode(of = "freeBoardIdx")
@@ -22,12 +23,12 @@ public class FreeBoard {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long freeBoardIdx;
-    @Column(length = 200)
-    private String freeBoardImg;
     @Column(nullable = false, length = 50)
     private String freeBoardTitle;
     @Column(nullable = false, length = 2000)
     private String freeBoardContent;
+    @Column(length = 200)
+    private String freeBoardImg;
     @Column(nullable = false, length = 20)
     private String freeBoardLocation;
     @Column(nullable = false, length = 20)
@@ -36,13 +37,13 @@ public class FreeBoard {
     private LocalDateTime freeBoardWriteDate;
     @UpdateTimestamp
     private LocalDateTime freeBoardUpdateDate;
-    @ColumnDefault("0")
-    private Long freeBoardViewCount;
-    @ColumnDefault("0")
-    private Long freeBoardReplyCount;
-    @ColumnDefault("0")
-    private Long freeBoardLikeCount;
-
-    @Column(nullable = false)
-    private Long memberIdx;
+    @Builder.Default
+    private Long freeBoardViewCount = 0L;
+    @Builder.Default
+    private Long freeBoardReplyCount = 0L;
+    @Builder.Default
+    private Long freeBoardLikeCount = 0L;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_idx", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Member member;
 }
