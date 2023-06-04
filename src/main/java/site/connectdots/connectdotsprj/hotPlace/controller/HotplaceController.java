3f03d96713a363a151564ddf82ff2d0f84831e25
sumin <330sum@naver.com>
2023-06-04 17:15:44 +0900

    :white_check_mark: [feature] Hotplace Test 1. 벌크 데이터 생성 2. 전체조회 테스트 완료 3. 개별조회 테스트 완료

---
+++ src/main/java/site/connectdots/connectdotsprj/hotPlace/controller/HotplaceController.java
@@ -0,0 +1,22 @@
+package site.connectdots.connectdotsprj.hotPlace.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import site.connectdots.connectdotsprj.hotPlace.entity.Hotplace;
+import site.connectdots.connectdotsprj.hotPlace.service.HotplaceService;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/contents/hot-place")
+public class HotplaceController {
+
+    private final HotplaceService hotplaceService;
+
+}
--- src/main/java/site/connectdots/connectdotsprj/hotPlace/entity/HotplaceLocation.java
+++ src/main/java/site/connectdots/connectdotsprj/hotPlace/entity/HotplaceLocation.java
@@ -1,8 +1,17 @@
 package site.connectdots.connectdotsprj.hotPlace.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum HotplaceLocation {
 
     강남구, 강동구, 강북구, 강서구, 관악구, 광진구, 구로구, 금천구, 노원구, 도봉구,
     동대문구, 동작구, 마포구, 서대문구, 서초구, 성동구, 성북구, 송파구, 양천구,
     영등포구, 용산구, 은평구, 종로구, 중구, 중랑구
+
+
+//    GANGNAM_GU, GANGDONG_GU, GANGBUK_GU, GANGSEO_GU, GANWAN_GU, GWANGJIN_GU, GURO_GU, GEUMCHEON_GU, NOWON_GU, DOBONG_GU,
+//    DONGDAEMUN_GU, DONGJAK_GU, MAPO_GU, SEODAEMUN_GU, SEOCHO_GU, SEONGDONG_GU, SEONGBUK_GU, SONGPA_GU, YANGCHEON_GU,
+//    YEONGDEUNGPO_GU, YONGSAN_GU, EUNPYEONG_GU, JONGNO_GU, JUNG_GU, JUNGNANG_GU
+
 }
