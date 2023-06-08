fc78b2ca437b7652184af2c73615aa5a49c58be7
lkdcode <dlrlejr1@naver.com>
2023-06-08 23:42:38 +0900

    :white_check_mark: [feature] 멤버 회원가입 이메일 인증 구현
    1. YAML 설정은 로컬에 맞게 셋팅해줘야한다.

--- src/main/java/site/connectdots/connectdotsprj/member/controller/api/MemberController.java
+++ src/main/java/site/connectdots/connectdotsprj/member/controller/api/MemberController.java
@@ -17,5 +17,4 @@ public class MemberController {
         return memberService.signUp(dto);
     }
 
-
 }
---
+++ src/main/java/site/connectdots/connectdotsprj/memberemail/controller/MemberEmailController.java
@@ -0,0 +1,25 @@
+package site.connectdots.connectdotsprj.memberemail.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import site.connectdots.connectdotsprj.memberemail.service.MemberEmailService;
+import site.connectdots.connectdotsprj.memberemail.dto.MemberEmailCheckRequest;
+
+import javax.mail.MessagingException;
+import java.io.UnsupportedEncodingException;
+
+@RestController
+@RequestMapping("/connects/sign-up")
+@RequiredArgsConstructor
+public class MemberEmailController {
+
+    private final MemberEmailService emailService;
+
+    @ResponseBody
+    @PostMapping("/mail")        // 이 부분은 각자 바꿔주시면 됩니다.
+    public ResponseEntity<String> EmailCheck(@RequestBody MemberEmailCheckRequest emailCheckReq) throws MessagingException, UnsupportedEncodingException {
+        String authCode = emailService.sendEmail(emailCheckReq.getEmail());
+        return ResponseEntity.ok().body(authCode);    // Response body에 값을 반환해줄게요~
+    }
+}
