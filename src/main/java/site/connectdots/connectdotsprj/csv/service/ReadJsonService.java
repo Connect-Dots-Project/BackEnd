09b93c4517c52ec7bc8edced422c79c3dc344dac
JeFeel <jaephil94@gmail.com>
2023-06-09 17:16:37 +0900

    [feature] 파일명 변경

--- src/main/java/site/connectdots/connectdotsprj/csv/service/ReadJsonService.java
+++ src/main/java/site/connectdots/connectdotsprj/csv/service/ReadJsonService.java
@@ -2,10 +2,9 @@ package site.connectdots.connectdotsprj.csv.service;
 
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
-import site.connectdots.connectdotsprj.csv.entity.JsonData;
+import site.connectdots.connectdotsprj.csv.entity.CsvData;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -15,9 +14,9 @@ import java.util.List;
 @Service
 public class ReadJsonService {
 
-    public List<JsonData> readJsonFile() throws IOException {
+    public List<CsvData> readJsonFile() throws IOException {
 //        3개의 파일을 읽어서 하나로 합침
-        List<JsonData> csvMergedData = new ArrayList<>();
+        List<CsvData> csvMergedData = new ArrayList<>();
 
         String[] jsonFilePath = {
                 "C:\\BackEnd-fork\\src\\main\\resources\\static\\json_csv\\GS25.json",
@@ -28,7 +27,7 @@ public class ReadJsonService {
         // ObjectMapper를 사용하여 JSON 파일을 읽고 Product 리스트로 변환
         ObjectMapper objectMapper = new ObjectMapper();
         for (String s : jsonFilePath) {
-            List<JsonData> data =
+            List<CsvData> data =
                     objectMapper.readValue(new File(s), new TypeReference<>() {});
             csvMergedData.addAll(data);
         }
