05136290f8a8e97e230ad0db925d4416144f818d
lkdcode <dlrlejr1@naver.com>
2023-06-10 22:34:33 +0900

    :sparkles: [initial] naver login initial commit

--- src/main/java/site/connectdots/connectdotsprj/naverlogin/controller/api/NaverLoginController.java
+++ src/main/java/site/connectdots/connectdotsprj/naverlogin/controller/api/NaverLoginController.java
@@ -1,28 +1,9 @@
 package site.connectdots.connectdotsprj.naverlogin.controller.api;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import site.connectdots.connectdotsprj.naverlogin.service.NaverLoginService;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
 
 @RestController
 @RequiredArgsConstructor
 public class NaverLoginController {
-    private final NaverLoginService loginService;
-
-    @GetMapping("/naver-login")
-    public void naverLogin(HttpServletRequest request, HttpServletResponse response) throws MalformedURLException, UnsupportedEncodingException, URISyntaxException {
-        String url = loginService.getNaverAuthorizeUrl("authorize");
-        try {
-            response.sendRedirect(url);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
