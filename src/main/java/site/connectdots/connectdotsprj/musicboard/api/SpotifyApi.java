63cef2eb3b2c55f540af84536306759ac2ed5784
qwr133 <ksg165000@gmail.com>
2023-06-15 15:25:36 +0900

    [feature] spotifyAPI 트랙 리스트 가져오기

--- src/main/java/site/connectdots/connectdotsprj/musicboard/api/SpotifyApi.java
+++ src/main/java/site/connectdots/connectdotsprj/musicboard/api/SpotifyApi.java
@@ -3,10 +3,7 @@ package site.connectdots.connectdotsprj.musicboard.api;
 
 import net.bytebuddy.matcher.StringMatcher;
 import org.apache.hc.core5.http.ParseException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import se.michaelthelin.spotify.SpotifyHttpManager;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
@@ -19,13 +16,11 @@ import se.michaelthelin.spotify.requests.data.tracks.GetTrackRequest;
 
 import java.io.IOException;
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
 
 @RestController
+@CrossOrigin
 public class SpotifyApi {
 
     private static final String clientId = "e665029ca3b34c27b937c214233fd932";
@@ -49,73 +44,132 @@ public class SpotifyApi {
     @GetMapping("/contents/music-board")
     public void oauth(@RequestParam String code) throws IOException, ParseException, SpotifyWebApiException {
 
-//        System.out.println("code = [" + code + "]");
-
         AuthorizationCodeRequest authorizationCodeRequest = spotifyApi.authorizationCode(code).build();
         final AuthorizationCodeCredentials credentials = authorizationCodeRequest.execute();
-//        System.out.println("credentials = [" + credentials + "]");
 
         spotifyApi.setAccessToken(credentials.getAccessToken());
         spotifyApi.setRefreshToken(credentials.getRefreshToken());
 
-//        System.out.println("Expires in: " + credentials.getExpiresIn());
 
-        final Paging<Category> categoryPaging = spotifyApi.getListOfCategories().build().execute();
-
-
-        final Playlist playlist = spotifyApi.getPlaylist("37i9dQZEVXbNxXF4SkHj9F").build().execute();
-
-        // 플레이리스트 ID 값을 저장할 리스트
-        List<String> playlistIds = new ArrayList<>();
-
-        Paging<PlaylistTrack> tracks = playlist.getTracks();
-        PlaylistTrack[] playlistTracks = tracks.getItems();
-        Image[] images = playlist.getImages();
-
-        for (Image image : images) {
-            String url = image.getUrl();
-            System.out.println("url = " + url);
-        }
+        Map<String, String> playlistIds = new HashMap<>();
+        playlistIds.put("HotHitsKorea50", "37i9dQZF1DWT9uTRZAYj0c");
+        playlistIds.put("top50Global", "37i9dQZEVXbMDoHDwVN2tF");
+        playlistIds.put("viralHitKorea", "37i9dQZF1DWWwaxRea1LWS");
+        playlistIds.put("LeSeraphim", "5wBovveXvw0woKL72NqA0P");
+        playlistIds.put("BoBBY", "6OlqhvuJE4cdphMx610BtM");
+        playlistIds.put("StrayKids", "37i9dQZF1DWTY99d0AYptp");
+        playlistIds.put("KidWine", "37i9dQZF1EIUCUQCE4UP0F");
+        playlistIds.put("BTS", "0WOCvglRdeDmMnt8zFV0vc");
+        playlistIds.put("Monday", "37i9dQZF1DXbhErEye1cVu");
+        playlistIds.put("HappyHits", "37i9dQZF1DXbhErEye1cVu");
 
 
-        // 플레이리스트 ID 값을 변수에 저장
-        for (PlaylistTrack playlistTrack : playlistTracks) {
-            IPlaylistItem track = playlistTrack.getTrack();
-            playlistIds.add(track.getId());
-        }
+        for (Map.Entry<String, String> entry : playlistIds.entrySet()) {
+            final Playlist playlist = spotifyApi.getPlaylist(entry.getValue()).build().execute();
+            String name = playlist.getName();
+            Image[] images = playlist.getImages();
+            System.out.println(name);
+            for (Image image : images) {
+                String url = image.getUrl();
+                System.out.println("url = " + url);
+            }
 
-        // 랜덤으로 10개 선택
-        Random random = new Random();
-        List<String> randomPlaylistIds = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            int randomIndex = random.nextInt(playlistIds.size());
-            String randomPlaylistId = playlistIds.get(randomIndex);
-            randomPlaylistIds.add(randomPlaylistId);
-        }
-
-        // 선택된 플레이리스트 ID 출력
-        for (String playlistId : randomPlaylistIds) {
-            System.out.println("Playlist ID: " + playlistId);
-        }
-
-//        Paging<PlaylistTrack> tracks = playlist.getTracks();
-//        PlaylistTrack[] playlistTracks = tracks.getItems();
-
-
-
-        for (PlaylistTrack playlistTrack : playlistTracks) {
-            IPlaylistItem track = playlistTrack.getTrack();
-            String name = track.getName();
-            System.out.println("name = " + name); //제목
-
-            ExternalUrl externalUrls = track.getExternalUrls();
-            Map<String, String> externalUrls1 = externalUrls.getExternalUrls();
-
-            System.out.println("externalUrls1 = " + externalUrls1);
+            // 해당 플레이리스트의 트랙 정보 가져오기
+            Paging<PlaylistTrack> playlistTracksInfo = playlist.getTracks();
+            PlaylistTrack[] playlistTracksArray = playlistTracksInfo.getItems();
+            for (PlaylistTrack playlistTrack : playlistTracksArray) {
+                IPlaylistItem track = playlistTrack.getTrack();
+                System.out.println("Track Name: " + track.getName());
+            }
 
         }
-
-
     }
 }
+//
+//    @GetMapping("/contents/music-board/{playlistName}")
+//    public void oauth(@RequestParam String code, @RequestParam String playlistName) throws
+//            IOException, ParseException, SpotifyWebApiException {
+//
+//
+//        AuthorizationCodeRequest authorizationCodeRequest = spotifyApi.authorizationCode(code).build();
+//        final AuthorizationCodeCredentials credentials = authorizationCodeRequest.execute();
+//
+//        spotifyApi.setAccessToken(credentials.getAccessToken());
+//        spotifyApi.setRefreshToken(credentials.getRefreshToken());
+//
+//        Map<String, String> playlistIds = new HashMap<>();
+//        playlistIds.put("HotHitsKorea50", "37i9dQZF1DWT9uTRZAYj0c");
+//        playlistIds.put("top50Global", "37i9dQZEVXbMDoHDwVN2tF");
+//        playlistIds.put("viralHitKorea", "37i9dQZF1DWWwaxRea1LWS");
+//        playlistIds.put("LeSeraphim", "5wBovveXvw0woKL72NqA0P");
+//        playlistIds.put("BoBBY", "6OlqhvuJE4cdphMx610BtM");
+//        playlistIds.put("StrayKids", "37i9dQZF1DWTY99d0AYptp");
+//        playlistIds.put("KidWine", "37i9dQZF1EIUCUQCE4UP0F");
+//        playlistIds.put("BTS", "0WOCvglRdeDmMnt8zFV0vc");
+//        playlistIds.put("Monday", "37i9dQZF1DXbhErEye1cVu");
+//        playlistIds.put("HappyHits", "37i9dQZF1DXbhErEye1cVu");
+//
+//        String playlistId = playlistIds.get(playlistName);
+//        if (playlistId != null) {
+//            final Playlist playlist = spotifyApi.getPlaylist(playlistId).build().execute();
+//            String name = playlist.getName();
+//            Image[] images = playlist.getImages();
+//            System.out.println(name);
+//            for (Image image : images) {
+//                String url = image.getUrl();
+//                System.out.println("url = " + url);
+//            }
+//
+//            // 해당 플레이리스트의 트랙 정보 가져오기
+//            Paging<PlaylistTrack> playlistTracksInfo = playlist.getTracks();
+//            PlaylistTrack[] playlistTracksArray = playlistTracksInfo.getItems();
+//            for (PlaylistTrack playlistTrack : playlistTracksArray) {
+//                IPlaylistItem playlistItem = playlistTrack.getTrack();
+//                if (playlistItem instanceof Track) {
+//                    Track track = (Track) playlistItem;
+//                    System.out.println("Track Name: " + track.getName());
+//                    ArtistSimplified[] artists1 = track.getArtists();
+//                    System.out.println("artist : "+ artists1 );
+
+                    // 앨범 정보 가져오기
+//                    AlbumSimplified album = track.getAlbum();
+//                    ArtistSimplified[] artists = track.getArtists();
+//                    System.out.println("artitst = " + artists);
+//                    String albumName = album.getName();
+//                    Image[] albumImages = album.getImages();
+//
+//                    System.out.println("Album Name: " + albumName);
+//                    for (Image image : albumImages) {
+//                        String imageUrl = image.getUrl();
+//                        System.out.println("Image URL: " + imageUrl);
+//                    }
+
+                    // 추가적인 작업 수행...
+
+
+
+
+//        List<String> playlistIds = Arrays.asList("37i9dQZF1DWT9uTRZAYj0c", "37i9dQZEVXbMDoHDwVN2tF", "37i9dQZF1DWWwaxRea1LWS," +
+//                                           "5wBovveXvw0woKL72NqA0P", "6OlqhvuJE4cdphMx610BtM", "37i9dQZF1DWTY99d0AYptp", "37i9dQZF1EIUCUQCE4UP0F"
+//                                        ,"0WOCvglRdeDmMnt8zFV0vc", "37i9dQZF1DXbhErEye1cVu", "37i9dQZF1DXdPec7aLTmlC"
+//        );
+//
+//        for (String playlistId : playlistIds) {
+//            final Playlist playlist = spotifyApi.getPlaylist(playlistId).build().execute();
+//            String name = playlist.getName();
+//            Image[] images = playlist.getImages();
+//            System.out.println(name);
+//            for (Image image : images) {
+//                String url = image.getUrl();
+//                System.out.println("url = " + url);
+//            }
+//        }
+
+
+
+
+
+
+
+
 
