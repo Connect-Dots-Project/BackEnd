65c99b4b0d905d7dd1234a03bad0861310e97910
qwr133 <ksg165000@gmail.com>
2023-06-21 20:53:15 +0900

    :recycle: [feature] spotifyPlaylist final 6/21

--- src/main/java/site/connectdots/connectdotsprj/musicboard/dto/response/MusicListResponseDTO.java
+++ src/main/java/site/connectdots/connectdotsprj/musicboard/dto/response/MusicListResponseDTO.java
@@ -1,12 +1,8 @@
 package site.connectdots.connectdotsprj.musicboard.dto.response;
 
 import lombok.*;
-<<<<<<< HEAD
-import site.connectdots.connectdotsprj.musicboard.entity.Music;
-=======
 
-import javax.persistence.Column;
->>>>>>> b4b5e409c0b6a1d7557221fdba145a9c21e6da79
+
 
 @Getter
 @ToString
@@ -17,24 +13,6 @@ import javax.persistence.Column;
 public class MusicListResponseDTO {
 
     private Long musicBoardIdx;
-<<<<<<< HEAD
-    private String musicBoardTrack;
-    private String musicBoardTrackImg;
-    private String musicBoardTitle;
-    private String musicBoardTitleImg;
-    private String musicBoardSinger;
-    private  Long musicBoardViewCount;
-
-    public MusicListResponseDTO(Music music){
-        this.musicBoardIdx = music.getMusicBoardIdx();
-        this.musicBoardTrack=music.getMusicBoardTrack();
-        this.musicBoardTrackImg=music.getMusicBoardTrackImg();
-        this.musicBoardTitle=music.getMusicBoardTitle();
-        this.musicBoardTitleImg= music.getMusicBoardTitleImg();
-        this.musicBoardSinger=music.getMusicBoardSinger();
-        this.musicBoardViewCount=music.getMusicBoardViewCount();
-    }
-=======
 
     private String spotifyMusicId;
 
@@ -45,7 +23,6 @@ public class MusicListResponseDTO {
     private String musicBoardTitleImage;
 
     private String musicBoardPreviewUrl;
->>>>>>> b4b5e409c0b6a1d7557221fdba145a9c21e6da79
 
     private String musicBoardTrack;
     private String musicBoardTrackImage;
