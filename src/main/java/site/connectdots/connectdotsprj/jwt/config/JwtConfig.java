365a5f6b48cbbce1a26c7e209298156a55f995a5
lkdcode <dlrlejr1@naver.com>
2023-06-26 00:19:57 +0900

    :white_check_mark: [feat] 자유게시판 CRUD 구현 중

--- src/main/java/site/connectdots/connectdotsprj/jwt/config/JwtConfig.java
+++ src/main/java/site/connectdots/connectdotsprj/jwt/config/JwtConfig.java
@@ -17,12 +17,12 @@ public class JwtConfig implements WebMvcConfigurer {
         // TODO : URL 설정해야 함
         registry
                 .addInterceptor(jwtTokenInterceptor)
-
-                .addPathPatterns("/**")
-                .excludePathPatterns("/") // TODO : 메인페이지는 서버에서 알 수 없다. 이 경로가 맞나?
-                .excludePathPatterns("/jwt/test/get-token")
-                .excludePathPatterns("/connects/sign-up")
-                .excludePathPatterns("/connects/login")
+//                .addPathPatterns("/**")
+                .excludePathPatterns("/**") // TODO : 메인페이지는 서버에서 알 수 없다. 이 경로가 맞나?
+//                .excludePathPatterns("/jwt/test/get-token")
+//                .excludePathPatterns("/connects/sign-up")
+//                .excludePathPatterns("/contents/free-board/**")
+//                .excludePathPatterns("/connects/login")
         ;
     }
 }
