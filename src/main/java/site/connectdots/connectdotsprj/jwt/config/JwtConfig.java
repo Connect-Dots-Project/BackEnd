c60860db3c1d2c7efe280297826534aede10098f
lkdcode <dlrlejr1@naver.com>
2023-06-26 09:54:53 +0900

    :stethoscope: [test] 배포 테스트

--- src/main/java/site/connectdots/connectdotsprj/jwt/config/JwtConfig.java
+++ src/main/java/site/connectdots/connectdotsprj/jwt/config/JwtConfig.java
@@ -18,6 +18,7 @@ public class JwtConfig implements WebMvcConfigurer {
         registry
                 .addInterceptor(jwtTokenInterceptor)
                 .addPathPatterns("/**")
+                .excludePathPatterns("/health-check")
                 .excludePathPatterns("/") // TODO : 메인페이지는 서버에서 알 수 없다. 이 경로가 맞나?
                 .excludePathPatterns("/connects/sign-up")
                 .excludePathPatterns("/connects/login")
